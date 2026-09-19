9f88a248b753b448f8d106740f8dcab0f3658729
Vivek Singh <svivek00singh@gmail.com>
2026-09-19 20:17:41 +0530

    Fix GitHub Pages CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = [
-    "http://localhost:5173",
+   // "http://localhost:5173",
     "https://vivek-singh75.github.io"
 ];
 
