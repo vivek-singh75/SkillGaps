d29bdc668fdc9942306f7e31b2458fea8f6bae92
Vivek Singh <svivek00singh@gmail.com>
2026-09-19 19:33:50 +0530

    error solved

--- Frontend/src/App.routes.jsx
+++ Frontend/src/App.routes.jsx
@@ -1,29 +1,48 @@
-import {createBrowserRouter, Router} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+
 import Login from "./Features/auth/pages/Login.jsx";
 import Register from "./Features/auth/pages/Register.jsx";
 import Protected from "./Features/auth/components/Protected.jsx";
 import Home from "./Features/auth/pages/Home.jsx";
-import Interview from "./Features/auth/pages/Interview.jsx"
+import Interview from "./Features/auth/pages/Interview.jsx";
 import ResumeGenerator from "./Features/auth/pages/ResumeGenerator.jsx";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/login",
+            element: <Login />
+        },
+        {
+            path: "/register",
+            element: <Register />
+        },
+        {
+            path: "/",
+            element: (
+                <Protected>
+                    <Home />
+                </Protected>
+            )
+        },
+        {
+            path: "/interview/:interviewId",
+            element: (
+                <Protected>
+                    <Interview />
+                </Protected>
+            )
+        },
+        {
+            path: "/resume/download",
+            element: (
+                <Protected>
+                    <ResumeGenerator />
+                </Protected>
+            )
+        }
+    ],
     {
-        path : "/login",
-        element : <Login/>
-    },
-    {
-        path : "/register",
-        element : <Register/>
-    },{
-        path : "/",
-        element : (<Protected>{<Home/>}</Protected>)
-    },
-    {
-        path : "/interview/:interviewId",
-        element : (<Protected>{<Interview/>}</Protected>)
-    },
-    {
-        path : "/resume/download",
-        element : (<Protected>{<ResumeGenerator/>}</Protected>)
+        basename: "/SkillGaps"
     }
-])
+);
