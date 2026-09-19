66806b75c1150fbf10697af97d7e9952d6a29dbe
Vivek Singh <svivek00singh@gmail.com>
2026-09-19 19:57:51 +0530

    Configure GitHub Pages deployment

--- Frontend/src/App.routes.jsx
+++ Frontend/src/App.routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import Login from "./Features/auth/pages/Login.jsx";
 import Register from "./Features/auth/pages/Register.jsx";
@@ -7,42 +7,37 @@ import Home from "./Features/auth/pages/Home.jsx";
 import Interview from "./Features/auth/pages/Interview.jsx";
 import ResumeGenerator from "./Features/auth/pages/ResumeGenerator.jsx";
 
-export const router = createBrowserRouter(
-    [
-        {
-            path: "/login",
-            element: <Login />
-        },
-        {
-            path: "/register",
-            element: <Register />
-        },
-        {
-            path: "/",
-            element: (
-                <Protected>
-                    <Home />
-                </Protected>
-            )
-        },
-        {
-            path: "/interview/:interviewId",
-            element: (
-                <Protected>
-                    <Interview />
-                </Protected>
-            )
-        },
-        {
-            path: "/resume/download",
-            element: (
-                <Protected>
-                    <ResumeGenerator />
-                </Protected>
-            )
-        }
-    ],
+export const router = createHashRouter([
     {
-        basename: "/SkillGaps"
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/",
+        element: (
+            <Protected>
+                <Home />
+            </Protected>
+        )
+    },
+    {
+        path: "/interview/:interviewId",
+        element: (
+            <Protected>
+                <Interview />
+            </Protected>
+        )
+    },
+    {
+        path: "/resume/download",
+        element: (
+            <Protected>
+                <ResumeGenerator />
+            </Protected>
+        )
     }
-);
+]);
